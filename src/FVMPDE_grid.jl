5aff0fc75539554e96d17dea5be238efdf9de9d2
raptor <amir_1061@yahoo.com>
2021-12-23 15:55:23 +0100

    Redefine Grid and Problem

--- src/FVMPDE_grid.jl
+++ src/FVMPDE_grid.jl
@@ -20,14 +20,17 @@ mutable struct FVMPDEGrid
     X
     Y
     Z
+    nx
+    ny
+    nz
     dX
     dY
     dZ
     U
     Uboundries
     dimension::Type{<:Dimension}
-    function FVMPDEGrid(X,Y,Z,dX,dY,dZ,U,Uboundries,dimension)
-        return new(X,Y,Z,dX,dY,dZ,U,Uboundries,dimension)
+    function FVMPDEGrid(X,Y,Z,nx,ny,nz,dX,dY,dZ,U,Uboundries,dimension)
+        return new(X,Y,Z,nx,ny,nz,dX,dY,dZ,U,Uboundries,dimension)
     end
 end
 
@@ -35,31 +38,37 @@ function FVMPDEGrid(_X::AbstractVector, _Y::AbstractVector, _Z::AbstractVector)
     X = _X
     Y = _Y
     Z = _Z
+    nx = length(X)
+    ny = length(Y)
+    nz = length(Z)
     dX = _X[2:end] .- _X[1:end-1]
     dY = _Y[2:end] .- _Y[1:end-1]
     dZ = _Z[2:end] .- _Z[1:end-1]
     #Uboundries = zeros(length(_X),length(_Y))
     dimension = _2D
 
-    return FVMPDEGrid(X,Y,Z,dX,dY,dZ,nothing,nothing,dimension)
+    return FVMPDEGrid(X,Y,Z,nx,ny,nz,dX,dY,dZ,nothing,nothing,dimension)
 end
 
 function FVMPDEGrid(_X::AbstractVector, _Y::AbstractVector)
     X = _X
     Y = _Y
+    nx = length(X)
+    ny = length(Y)
     dX = _X[2:end] .- _X[1:end-1]
     dY = _Y[2:end] .- _Y[1:end-1]
     #Uboundries = zeros(length(_X),length(_Y))
     dimension = _2D
 
-    return FVMPDEGrid(X,Y,nothing,dX,dY,nothing,nothing,nothing,dimension)
+    return FVMPDEGrid(X,Y,[],nx,ny,0,dX,dY,nothing,nothing,nothing,dimension)
 end
 
 function FVMPDEGrid(_X::AbstractVector)
     X = _X
+    nx = length(X)
     dX = _X[2:end] .- _X[1:end-1]
     #Uboundries = zeros(length(_X))
     dimension = _1D
 
-    return FVMPDEGrid(X,nothing,nothing,dX,nothing,nothing,nothing,nothing,dimension)
+    return FVMPDEGrid(X,[],[],nx,0,0,dX,nothing,nothing,nothing,nothing,dimension)
 end
--- src/FVMPDE_problem.jl
+++ src/FVMPDE_problem.jl
@@ -2,6 +2,8 @@
 ##########################################################
 
 mutable struct FVMPDEProblem
+    grid::FVMPDEGrid
+
     """
     Flux function F in x direction
 
@@ -53,7 +55,16 @@ mutable struct FVMPDEProblem
         @assert size(G(U0)) == size(U0)
         @assert size(H(U0)) == size(U0)
         @assert size(S(U0)) == size(U0)
-
+        @assert D == grid.dimension
+        @assert length(size(U0))-1 == checkDim(D)
+        @assert size(U0,1) == length(grid.X)
+        if D == _2D || D == _3D
+            @assert size(U0,2) == length(grid.Y)
+        end
+        if D == _3D
+            @assert size(U0,3) == length(grid.Z)
+        end
+        grid.U = copy(U0)
         θ = haskey(kwargs,:parameters) ? kwargs[:parameters] : nothing
 
         U_min = haskey(kwargs,:ul) ? kwargs[:ul] : -Inf * ones(size(U0))
@@ -62,12 +73,20 @@ mutable struct FVMPDEProblem
         @assert size(U_min) == size(U0)
         @assert size(U_max) == size(U0)
 
-        nvars = size(U0,1)
+        nvars = size(U0,checkDim(D)+1)
 
         #@assert checkDim(D) == size(U0,)
 
-        return new(F, G, H, S, nvars, θ, U0, U_min, U_max, D )
+        return new(grid,F, G, H, S, nvars, θ, U0, U_min, U_max, D )
     end
 
 
 end
+
+X = [0.0, 0.1, 0.3, 0.6, 1.0, 1.5]
+grid = FVMPDEGrid(X,X)
+
+myF(U) = zeros(size(U))
+u0=zeros(6,6,1)
+
+FVMPDEProblem(grid,u0,myF,func_G=myF)
