656edfcf60b86c6f327168f0ab7547061c9763d6
raptor <amir_1061@yahoo.com>
2022-01-19 21:34:27 +0100

    Fix a funny mistake

--- src/FVMPDE_methods.jl
+++ src/FVMPDE_methods.jl
@@ -59,90 +59,86 @@ function κ_Scheme(r,κ)
 end
 
 
-function uw1(u,grid::FVMPDEGrid,dim)
+function uw1(r)
     return zeros(size(r))
 end
 
-function uw2(u,grid::FVMPDEGrid,dim)
+function uw2(r)
     return ones(size(r))
 end
 
-function uw3(u,grid::FVMPDEGrid,dim)
+function uw3(r)
     return κ_Scheme(r,1/3)
 end
 
-function uw4(u,grid::FVMPDEGrid,dim)
+function uw4(r)
     return κ_Scheme(r,1/2)
 end
 
-function cd(u,grid::FVMPDEGrid,dim)
+function cd(r)
     return r
 end
 
-function fr(u,grid::FVMPDEGrid,dim)
+function fr(r)
     return κ_Scheme(r,0)
 end
 
-function kn(u,grid::FVMPDEGrid,dim)
+function kn(r)
     return @. max(0,min(2*r,min(1/3 + 2 /3 * r,2)))
 end
 
-function sb(u,grid::FVMPDEGrid,dim)
+function sb(r)
     return @. max(0,min(2 * r,1),min(r,2))
 end
 
-function mm(u,grid::FVMPDEGrid,dim)
+function mm(r)
     return @. max(0,min(r,1))
 end
 
-function mu(u,grid::FVMPDEGrid,dim)
+function mu(r)
     return @. max(0,min(2 * r,(r+1)/2,2))
 end
 
-function ha(u,grid::FVMPDEGrid,dim)
+function ha(r)
     return @. (r + abs(r))/(r+1)
 end
 
-function va1(u,grid::FVMPDEGrid,dim)
+function va1(r)
     return @. r*(r + 1)/(r^2+1)
 end
 
-function va2(u,grid::FVMPDEGrid,dim)
+function va2(r)
     return @. r*2/(r^2+1)
 end
 
-function vl(u,grid::FVMPDEGrid,dim)
+function vl(r)
     return @. (r + abs(r))/(1 + abs(r))
 end
 
-function op(u,grid::FVMPDEGrid,dim)
+function op(r)
     return @. 3*r*(r + 1)/(r^2+r+1)/2
 end
 
-function hc(u,grid::FVMPDEGrid,dim)
+function hc(r)
     return @. 1.5*(r + abs(r))/(r+2)
 end
 
-function hq(u,grid::FVMPDEGrid,dim)
+function hq(r)
     return @. 2*(r + abs(r))/(r+3)
 end
 
-function cm(u,grid::FVMPDEGrid,dim)
+function cm(r)
     return @. max(0,r*(3*r+1)/(r+1)^2)
 end
 
-function mc(u,grid::FVMPDEGrid,dim)
+function mc(r)
     return @. max(0,min(2*r,0.5*(r+1),2))
 end
 
-function sm(u,grid::FVMPDEGrid,dim)
+function sm(r)
     return @. max(0,min(2*r,(0.75*r+0.25),4))
 end
 
-function um(u,grid::FVMPDEGrid,dim)
+function um(r)
     return @. max(0,min(2*r,(0.25*r+0.75),2))
-end
-
-function gg(u,grid::FVMPDEGrid,dim)
-    return @. max(0,min(2*r,sqrt(r),2))
 end
