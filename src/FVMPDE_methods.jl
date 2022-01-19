8e5eee42ea9163d89977f46e0c2bded1f0da791c
raptor <amir_1061@yahoo.com>
2022-01-19 21:13:25 +0100

    FIx

--- src/FVMPDE_methods.jl
+++ src/FVMPDE_methods.jl
@@ -60,111 +60,89 @@ end
 
 
 function uw1(u,grid::FVMPDEGrid,dim)
-    f(r) = zeros(size(r))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return zeros(size(r))
 end
 
 function uw2(u,grid::FVMPDEGrid,dim)
-    f(r) = ones(size(r))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return ones(size(r))
 end
 
 function uw3(u,grid::FVMPDEGrid,dim)
-    f(r) = κ_Scheme(r,1/3)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return κ_Scheme(r,1/3)
 end
 
 function uw4(u,grid::FVMPDEGrid,dim)
-    f(r) = κ_Scheme(r,1/2)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return κ_Scheme(r,1/2)
 end
 
 function cd(u,grid::FVMPDEGrid,dim)
-    f(r) = r
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return r
 end
 
 function fr(u,grid::FVMPDEGrid,dim)
-    f(r) = κ_Scheme(r,0)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return κ_Scheme(r,0)
 end
 
 function kn(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2*r,min(1/3 + 2 /3 * r,2)))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2*r,min(1/3 + 2 /3 * r,2)))
 end
 
 function sb(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2 * r,1),min(r,2))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2 * r,1),min(r,2))
 end
 
 function mm(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(r,1))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(r,1))
 end
 
 function mu(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2 * r,(r+1)/2,2))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2 * r,(r+1)/2,2))
 end
 
 function ha(u,grid::FVMPDEGrid,dim)
-    f(r) = @. (r + abs(r))/(r+1)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. (r + abs(r))/(r+1)
 end
 
 function va1(u,grid::FVMPDEGrid,dim)
-    f(r) = @. r*(r + 1)/(r^2+1)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. r*(r + 1)/(r^2+1)
 end
 
 function va2(u,grid::FVMPDEGrid,dim)
-    f(r) = @. r*2/(r^2+1)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. r*2/(r^2+1)
 end
 
 function vl(u,grid::FVMPDEGrid,dim)
-    f(r) = @. (r + abs(r))/(1 + abs(r))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. (r + abs(r))/(1 + abs(r))
 end
 
 function op(u,grid::FVMPDEGrid,dim)
-    f(r) = @. 3*r*(r + 1)/(r^2+r+1)/2
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. 3*r*(r + 1)/(r^2+r+1)/2
 end
 
 function hc(u,grid::FVMPDEGrid,dim)
-    f(r) = @. 1.5*(r + abs(r))/(r+2)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. 1.5*(r + abs(r))/(r+2)
 end
 
 function hq(u,grid::FVMPDEGrid,dim)
-    f(r) = @. 2*(r + abs(r))/(r+3)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. 2*(r + abs(r))/(r+3)
 end
 
 function cm(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,r*(3*r+1)/(r+1)^2)
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,r*(3*r+1)/(r+1)^2)
 end
 
 function mc(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2*r,0.5*(r+1),2))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2*r,0.5*(r+1),2))
 end
 
 function sm(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2*r,(0.75*r+0.25),4))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2*r,(0.75*r+0.25),4))
 end
 
 function um(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2*r,(0.25*r+0.75),2))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2*r,(0.25*r+0.75),2))
 end
 
 function gg(u,grid::FVMPDEGrid,dim)
-    f(r) = @. max(0,min(2*r,sqrt(r),2))
-    return GeneralFluxLimiter(f,u,grid,dim)
+    return @. max(0,min(2*r,sqrt(r),2))
 end
